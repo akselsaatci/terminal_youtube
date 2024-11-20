652ba2e6cb6d252f5f01c6b8687eb6fa27739488
akselsaatci <100696686+akselsaatci@users.noreply.github.com>
2024-11-20 22:24:24 +0300

    adding freqCount

--- pkg/huffman_encoding.go
+++ pkg/huffman_encoding.go
@@ -1,3 +1,25 @@
 package huffman
 
-func encode(input string) {}
+func calculateFrequancy(input string) map[rune]int {
+
+	res := map[rune]int{}
+	for _, value := range input {
+		if _, ok := res[value]; ok {
+			res[value] += 1
+		} else {
+			res[value] = 1
+		}
+	}
+
+	return res
+}
+
+func Encode(input string) string {
+
+	freqTable := calculateFrequancy(input)
+
+	return "asdasd"
+
+}
+
+func Decode(input string) {}
--- pkg/huffman_encoding_test.go
+++ pkg/huffman_encoding_test.go
@@ -1,1 +1,39 @@
-package huffman_test
+package huffman
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestEncodeString(t *testing.T) {
+
+	input := "ssssssssdadasddsfsad"
+
+	expectedOutput := "000000001110111101011110100010111"
+
+	actualOutput := Encode(input)
+
+	if actualOutput != expectedOutput {
+		t.Fatalf(`huffman.Encode("%s") Expected Output = %s Actual Output = %s`, input, expectedOutput, actualOutput)
+	}
+}
+
+func TestDecodeString(t *testing.T) {}
+
+func TestFrequencyCount(t *testing.T) {
+
+	input := "aaadff"
+
+	expectedOutput := map[rune]int{
+		'a': 3,
+		'd': 1,
+		'f': 2,
+	}
+
+	actualOutput := calculateFrequancy(input)
+
+	if !reflect.DeepEqual(expectedOutput, actualOutput) {
+		t.Fatalf(`huffman.frequencyCount("%s") Expected Output = %q Actual Output = %q`, input, expectedOutput, actualOutput)
+	}
+
+}
