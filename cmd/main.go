8b6f0ef452e68ff9a9f8489c393e5aea5a49fb86
akselsaatci <100696686+akselsaatci@users.noreply.github.com>
2025-01-12 14:11:55 +0300

    feat: adding argparsing for cli usage

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 	"time"
 
 	"github.com/akselsaatci/terminal_youtube/pkg/image_to_ascii"
@@ -17,10 +18,20 @@ func main() {
 	frameDoneChannel := make(chan bool, 1)
 	frameRate := 24
 
+	if len(os.Args) != 2 {
+		log.Fatalf("You should provide a video path to run the program!")
+	}
+
+	videoPath := os.Args[1]
+
+	if len(videoPath) == 0 {
+		log.Fatalf("The videopath shouldn't be empty!")
+	}
+
 	brightnessStrategy := image_to_ascii.AvarageBrightnessStrategyConcurrent{}
 	converter := image_to_ascii.NewAsciiConverter(&brightnessStrategy)
 	downloader := yt_video_downloader.NewYtDipVideoDownloader("144")
-	downloadOut, err := downloader.DownloadToStdout("https://www.youtube.com/watch?v=QC8iQqtG0hg")
+	downloadOut, err := downloader.DownloadToStdout(videoPath)
 
 	renderer := renderer.NewTerminalRenderer(frameRate, framesChannel, doneChannel, frameDoneChannel)
 	defer downloadOut.Close()
@@ -35,7 +46,6 @@ func main() {
 	//for buffering
 	time.Sleep(5 * time.Second)
 	go renderer.Render()
-	//TODO should make a way to end
 	<-doneChannel
 
 }
